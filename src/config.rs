80de64c7bccf6b496a393547e41e3f1626933154
Brian Chan <i@b123400.net>
2023-12-29 21:51:28 +0900

    Actually connect with cert

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,10 @@
 use core::time::Duration;
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
+use rustls::{Certificate, PrivateKey, RootCertStore};
 use serde::Deserialize;
-use std::fs;
+use std::fs::{read_to_string, File};
+use std::io::BufReader;
 use std::net::Ipv4Addr;
 
 #[derive(Deserialize, Clone, Debug)]
@@ -13,8 +15,6 @@ pub struct MqttOptions {
     pub password: Option<String>,
 
     pub ca_file: Option<String>,
-
-    pub tls_insecure: Option<bool>,
     pub key_file: Option<String>,
     pub cert_file: Option<String>,
 
@@ -76,7 +76,7 @@ pub enum ConfigError {
 }
 
 pub fn read_from_default_location() -> Result<Config, ConfigError> {
-    let text = fs::read_to_string("zika_config.json").map_err(ConfigError::IOError)?;
+    let text = read_to_string("zika_config.json").map_err(ConfigError::IOError)?;
     let deserialized: Config = serde_json::from_str(&text).map_err(ConfigError::DecodeError)?;
     return Ok(deserialized);
 }
@@ -92,8 +92,6 @@ impl MqttOptions {
             password: another_option.password.or(self.password.clone()),
 
             ca_file: another_option.ca_file.or(self.ca_file.clone()),
-
-            tls_insecure: another_option.tls_insecure.or(self.tls_insecure),
             key_file: another_option.key_file.or(self.key_file.clone()),
             cert_file: another_option.cert_file.or(self.cert_file.clone()),
 
@@ -119,7 +117,7 @@ impl MqttBroker {
             (None, o) => o,
             (o, None) => o.clone(),
         };
-        // TODO: more options
+
         if let Some(opts) = mqtt_options {
             options.set_keep_alive(Duration::new(opts.keepalive_interval.unwrap_or(60), 0));
             options.set_topic_alias_max(opts.topic_alias_max);
@@ -128,41 +126,50 @@ impl MqttBroker {
                 options.set_credentials(u, p);
             };
 
-            use std::fs::File;
-            use std::io::Read;
-            use std::io::BufReader;
+            if opts.ca_file.is_some() || (opts.cert_file.is_some() && opts.key_file.is_some()) {
+                let mut root_cert_store = RootCertStore::empty();
+                if let Some(ca_file_path) = opts.ca_file {
+                    let ca_file = File::open(ca_file_path).unwrap();
+                    let mut ca_buf_read = BufReader::new(ca_file);
+                    let cas = rustls_pemfile::certs(&mut ca_buf_read);
+                    for ca in cas.into_iter() {
+                        root_cert_store
+                            .add(&Certificate(ca.unwrap().to_vec()))
+                            .unwrap();
+                    }
+                }
 
-            let mut cert_file = File::open("/Users/b123400/github/zika/cert.pem").unwrap();
-            let mut key = File::open("/Users/b123400/github/zika/key.pem").unwrap();
-            // let mut cert_buffer = Vec::new();
-            // let mut key_buffer = Vec::new();
+                let tls_config = match (opts.cert_file, opts.key_file) {
+                    (Some(cert_file_path), Some(key_file_path)) => {
+                        let cert_file = File::open(cert_file_path).unwrap();
+                        let key_file = File::open(key_file_path).unwrap();
+                        let mut cert_buf_read = BufReader::new(cert_file);
+                        let mut key_buf_read = BufReader::new(key_file);
 
-            let mut cert_buf_read = BufReader::new(cert_file);
+                        let certs = rustls_pemfile::certs(&mut cert_buf_read);
+                        let certs_vec = certs
+                            .into_iter()
+                            .map(|c| Certificate(c.unwrap().to_vec()))
+                            .collect();
 
-            let mut root_cert_store = rustls::RootCertStore::empty();
-            let certs = rustls_pemfile::certs(&mut cert_buf_read);
-            for cert in certs.into_iter() {
-                root_cert_store.add(&rustls::Certificate(cert.unwrap().to_vec())).unwrap();
+                        let mut keys = rustls_pemfile::ec_private_keys(&mut key_buf_read);
+                        let first_key = keys.next().unwrap();
+                        let key = PrivateKey(first_key.unwrap().secret_sec1_der().to_vec());
+
+                        rustls::ClientConfig::builder()
+                            .with_safe_defaults()
+                            .with_root_certificates(root_cert_store)
+                            .with_client_auth_cert(certs_vec, key)
+                            .unwrap()
+                    }
+                    _ => rustls::ClientConfig::builder()
+                        .with_safe_defaults()
+                        .with_root_certificates(root_cert_store)
+                        .with_no_client_auth(),
+                };
+
+                options.set_transport(rumqttc::Transport::tls_with_config(tls_config.into()));
             }
-
-            let tls_config = rustls::ClientConfig::builder()
-                .with_safe_defaults()
-                .with_root_certificates(root_cert_store)
-                .with_no_client_auth();
-
-            // read the whole file
-            // cert.read_to_end(&mut cert_buffer).unwrap();
-            // key.read_to_end(&mut key_buffer).unwrap();
-
-            // let tls_config = rumqttc::TlsConfiguration::SimpleNative {
-            //     ca: cert_buffer,
-            //     der: key_buffer,
-            //     password: "".to_string(),
-            // };
-            let transport = rumqttc::Transport::tls_with_config(
-                rumqttc::TlsConfiguration::Rustls(std::sync::Arc::new(tls_config))
-            );
-            options.set_transport(transport);
         };
         return options;
     }
